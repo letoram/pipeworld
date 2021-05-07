413bd8e4e93e45d6ca4663ae470aa60ab6dcaba3
arcanfe@nospam <arcanfe@nospam>
2021-05-07 11:42:11 +0000

    (cell:term) add listen- connection point
    This allows new clients to attach through a regular terminal and
    not just the cli-mode (where handover allocation works).
    
    FossilOrigin-Name: 04dcc405916746cbc4fc7019507c2900e52cbcd3ffc307b93ce14d39eb2ca026

--- pipeworld/cells/term.lua
+++ pipeworld/cells/term.lua
@@ -1,9 +1,40 @@
 local apply_fsrv_cell = system_load("cells/shared/fsrv.lua")()
 
+local function gen_name()
+	local res = {}
+	for i=1,8 do
+		res[i] = string.char(string.byte("a") + math.random(1, 10))
+	end
+
+	return table.concat(res, "")
+end
+
+local reopen
+reopen =
+function(cell)
+	local vid = target_alloc(cell.cp,
+		pipeworld_segment_handler(cell,
+			{
+				connected =
+				function()
+					reopen(cell)
+				end
+			}
+		)
+	)
+	link_image(vid, cell.bg)
+end
+
 return
 function(row, cfg, cmd)
 	local res = pipeworld_cell_template("cli", row, cfg)
 	local arg = cfg.terminal_arg and cfg.terminal_arg or ""
+	local name
+
+	if cfg.terminal_listen then
+		name = "pwterm_" .. gen_name()
+		arg = arg .. "env=ARCAN_CONNPATH=" .. name
+	end
 
 	if cmd and #cmd > 0 then
 		arg = string.format("%skeep_alive:autofit:exec=%s", #arg > 0 and ":" or "", cmd)
@@ -15,6 +46,11 @@ function(row, cfg, cmd)
 		end,
 	}))
 	apply_fsrv_cell(res, vid)
+
+	if name then
+		res.cp = name
+		reopen(res)
+	end
 
 	return res
 end
