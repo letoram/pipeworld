ca2a54ba0fe409772f2c0cf1321a2bfda330b6e1
arcanfe@nospam <arcanfe@nospam>
2021-05-07 11:50:23 +0000

    (wm) link/unlink action for row-popup- menu
    FossilOrigin-Name: 969f7c7b31f66c320ddbc44c4d430221a1ff15f85cb55afc13cfcef365f58661

--- pipeworld/menus/row.lua
+++ pipeworld/menus/row.lua
@@ -6,6 +6,9 @@ function()
 			label = "Delete Row", command = "/delete/row"
 		},
 		{
+			label = "Link/Unlink", command = "/link/row",
+		},
+		{
 			label = "Scale...", command = "/popup/row_scale_menu"
 		}
 	}
